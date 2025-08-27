a54f2ac2a6d9f7d5f328a85b236c1c35046218ab
violet <vdonhorore@gmail.com>
2025-08-27 09:51:52 +0200

    first commit

---
+++ violet.py
@@ -0,0 +1,17 @@
+#Variables are names given to data items that may take on one or more values during a program's runtime
+#x=5 where x is the variable that stores the value 5.
+#We define variables by assigning them a value using the assignment operator =
+x=5
+y="John"
+# The print function in Python is used to display output on the screen.
+# Here it displays what is stored in x and y
+# It takes one or more values,converts them to text and prints them to console
+print(x)
+print(y)
+#Constant in Python are variables that should remain unchanged throughout execution
+#Constant are written in upper case letters egPI=3.14159 or MAX_SCORE=100.
+#Constant help make code easier to understand and maintain
+PI=3.14159
+radius=5
+area=PI*radius*radius
+print(area)
